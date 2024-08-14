e765c25c22659db74274e56180514ba8172e59e6
Zain Dewsi <zain.dewsi@gmail.com>
2024-08-14 15:30:31 -0400

    buffer management

---
+++ lua/zvim/plugins/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "buffers",
+      separator_style = "slant",
+      offsets = {
+          {
+              filetype = "NvimTree"
+          }
+      }
+    },
+  },
+}
+
