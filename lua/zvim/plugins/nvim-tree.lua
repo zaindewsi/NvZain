ecd0e7da314f3686e05f8db20fb0f847f9b20d90
Zain Dewsi <zain.dewsi@gmail.com>
2024-08-14 14:46:49 -0400

    telescope, whichkey

--- lua/zvim/plugins/nvim-tree.lua
+++ lua/zvim/plugins/nvim-tree.lua
@@ -11,7 +11,6 @@ return {
     nvimtree.setup({
       view = {
         width = 35,
-        relativenumber = true,
       },
       -- change folder arrow icons
       renderer = {
@@ -49,7 +48,7 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<C-n>", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end
