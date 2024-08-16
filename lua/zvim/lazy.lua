0d7dcc4ecd17c3d56d97fcdf4c0baf38990021c8
Zain Dewsi <zain.dewsi@gmail.com>
2024-08-16 11:32:54 -0400

    lsp

--- lua/zvim/lazy.lua
+++ lua/zvim/lazy.lua
@@ -11,12 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("zvim.plugins", {
+require("lazy").setup({ { import = "zvim.plugins" }, { import = "zvim.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
   },
-    change_detection = {
-        notify = false,
-    },
+  change_detection = {
+    notify = false,
+  },
 })
